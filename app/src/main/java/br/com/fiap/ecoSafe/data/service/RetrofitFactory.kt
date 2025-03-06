7ec6b79967c55d0818d92bb60ab3d438760e8534
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-06 16:46:28 -0300

    feat: interceptor

--- app/src/main/java/br/com/fiap/ecoSafe/data/service/RetrofitFactory.kt
+++ app/src/main/java/br/com/fiap/ecoSafe/data/service/RetrofitFactory.kt
@@ -1,15 +1,26 @@
 package br.com.fiap.ecoSafe.data.service
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitFactory {
 
     private const val URL = "https://api.iucnredlist.org/api/v4/"
 
     private val retrofit: Retrofit by lazy {
+        val token = "Bearer Tbswz6LFLRHQACr5Teq52EiC2RWB1uQrZNnf"
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(token))
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         Retrofit.Builder()
             .baseUrl(URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
