10b29a121461f00e1be2a3f0d281dd95fe9bc903
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-06 21:06:59 -0300

    feat: service

--- app/src/main/java/br/com/fiap/ecoSafe/data/service/TaxaSisService.kt
+++ app/src/main/java/br/com/fiap/ecoSafe/data/service/TaxaSisService.kt
@@ -3,6 +3,7 @@ package br.com.fiap.ecoSafe.data.service
 import br.com.fiap.ecoSafe.data.model.ApiTaxSisResponse
 import br.com.fiap.ecoSafe.data.model.ApiKingdomResponse
 import br.com.fiap.ecoSafe.data.model.ApiResponseAssessment
+import br.com.fiap.ecoSafe.data.model.ApiResponsePhylum
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -12,13 +13,23 @@ interface TaxaSisService {
 
     @GET("taxa/sis/{sis_id}")
     fun getTaxon(
-        @Path("sis_id") sisId: Int,
-        @Header("Authorization") token: String
+        @Path("sis_id") sisId: Int
     ): Call<ApiTaxSisResponse>
 
     @GET("taxa/kingdom")
     fun getAllKingdoms(): Call<ApiKingdomResponse>
 
     @GET("taxa/kingdom/{kingdom}")
-    fun getKingdomData(@Path("kingdom") kingdom: String): Call<ApiResponseAssessment>
+    fun getKingdomData(
+        @Path("kingdom") kingdom: String
+    ): Call<ApiResponseAssessment>
+
+    @GET("taxa/phylum")
+    fun getPhylums()
+    : Call<ApiResponsePhylum>
+
+    @GET("taxa/phylum/{phylum}")
+    fun getPhylumData(
+        @Path("phylum") filo: String
+    ): Call<ApiResponseAssessment>
 }
